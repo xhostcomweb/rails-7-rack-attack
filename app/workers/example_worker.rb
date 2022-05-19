d290e88a854abb365848bc58a7f60a5d2dd8a342
Paul Anthony McGowan <xhostcom@gmail.com>
2022-05-19 04:13:40 +0100

    Update Sidekiq

---
+++ app/workers/example_worker.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+class ExampleWorker
+  include Sidekiq::Worker
+  sidekiq_options retry: false
+
+  #def perform
+   # Example.destroy_all
+ # end
+end
--- config/application.rb
+++ config/application.rb
@@ -9,7 +9,7 @@ require 'rails/all'
 
 Bundler.require(*Rails.groups)
 
-module JumpstartOcto
+module RackAttack
   class Application < Rails::Application # rubocop:disable Style/Documentation
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.0
